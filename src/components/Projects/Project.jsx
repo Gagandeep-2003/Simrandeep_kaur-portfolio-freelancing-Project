7abbb8d477efdfec2312fe5a5d83732e1d01e042
Gagandeep Singh <125819661+Gagandeep-2003@users.noreply.github.com>
2025-03-01 20:47:42 +0530

    Enhanced✨

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-// import { SlPaperPlane } from "react-icons/sl";
 import { data } from "../../data/work-data"; 
 import "./Projects.css";
 
-const Project = ({ title }) => (
+const Project = ({ title, subtitle }) => (
   <div className="page14-inner">
     <h1>{title}</h1>
-    {/* <SlPaperPlane className="i" /> */}
     <div className="center14"></div>
+    <span className="subtitle">{subtitle}</span> 
   </div>
 );
 
 const ProjectList = () => {
   return (
-  <>
+    <>
       <h2 className="heading-text" id="projects">
         Projects
       </h2>
-    <div id="page14">
-      {data.map((project) => (
-        <Project
-          key={project.id}
-          title={project.title}
-        />
-      ))}
-    </div>
-  </>
+      <div id="page14">
+        {data.map((project) => (
+          <Project
+            key={project.id}
+            title={project.title}
+            subtitle={project.subtitle}
+          />
+        ))}
+      </div>
+    </>
   );
 };
 

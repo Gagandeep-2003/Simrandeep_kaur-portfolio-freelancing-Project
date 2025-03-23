462d47fc502b606785a137f0eff02207b760f361
Gagandeep Singh <125819661+Gagandeep-2003@users.noreply.github.com>
2025-03-23 21:37:41 +0530

    Create skillsData.js

---
+++ src/data/skillsData.js
@@ -0,0 +1,62 @@
+export const data = [
+  {
+    name: "PCR",
+    imgName: "pcrTest",
+    bgColor: "#3297FE",
+  },
+  {
+    name: "DNA Sequencing",
+    imgName: "dna",
+    bgColor: "#C8E7F1",
+  },
+  {
+    name: "Molecular Cloning",
+    imgName: "bonding",
+    bgColor: "#291706",
+  },
+  {
+    name: "Biochemical Assays",
+    imgName: "biochemical",
+    bgColor: "#c2e0fe",
+  },
+  {
+    name: "Microbial Techniques",
+    imgName: "microbialCulture",
+    bgColor: "#CAF1DE",
+  },
+  {
+    name: "R Programming",
+    imgName: "rProgramming",
+    bgColor: "#2f2fa2",
+  },
+  {
+    name: "Python",
+    imgName: "python",
+    bgColor: "#C0C0C0",
+  },
+  {
+    name: "Molecular Biology",
+    imgName: "molecularBiology",
+    bgColor: "#AFEEEE",
+  },
+  {
+    name: "Nanodrop",
+    imgName: "nanodrop",
+    bgColor: "#E0E0E0",
+  },
+  {
+    name: "Microsoft Office",
+    imgName: "office",
+    bgColor: "#f8f9fa",
+  },
+  {
+    name: "Statistical Analysis",
+    imgName: "statisticalAnalysis",
+    bgColor: "#D3ECFF",
+  },
+  {
+    name: "Gene Expression Studies",
+    imgName: "geneExpressionAnalysis",
+    bgColor: "#AFEEEE",
+  },
+];

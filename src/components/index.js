93c48eb99add97023e58fea35fddbb807843cedc
Gagandeep Singh <125819661+Gagandeep-2003@users.noreply.github.com>
2025-03-23 21:07:14 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Project } from "./Projects/Project";
+export { default as TabSwitcher } from "./TabSwitcher/TabSwitcher";
+export { default as SpotlightSearch } from "./SpotlightSearch/SpotlightSearch";
+export { default as TabTitleChanger } from "./TabTitleChanger/TabTitleChanger";
+export { default as ShortcutPopup } from "./ShortcutPopup/ShortcutPopup";
